d11ff32e5d7fded1f84348f79f2052096d1100e5
Leonardo <leo.lima21051990@gmail.com>
2020-01-06 16:37:20 -0300

    face detection

--- src/camera_face.py
+++ src/camera_face.py
@@ -4,6 +4,7 @@
 import rospy
 import cv2
 import time
+import os
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 from std_msgs.msg import String
@@ -19,27 +20,28 @@ class camera:
     self.pub = rospy.Publisher('topic_camera_face', Image, queue_size=10)
     # bridge object
     self.bridge = CvBridge()
-    # classifier using haar cascade file
-    self.face_classifier = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 
   def callback(self, data):
     # convert img to cv2
     cv2_frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
     # gray convertion
     gray_cv2_frame = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2GRAY)
+    # classifier using haar cascade file
+    path = os.path.dirname(os.path.abspath(__file__))
+    face_classifier = cv2.CascadeClassifier(path+'/haarcascade_frontalface_default.xml')
     # face detection
-    det_faces = self.face_classifier.detectMultiScale(gray_cv2_frame, minSize=(50, 50))   
+    det_faces = face_classifier.detectMultiScale(gray_cv2_frame, minSize=(50, 50))       
     # draw retangle in faces
     for (x, y, largura, altura) in det_faces:	
-		  # Desenho do retangulo. No final cor e largura da borda
-		  cv2.rectangle(cv2_frame, (x,y), (x + largura, y + largura), (0, 0 , 255), 2)    
+      # Desenho do retangulo. No final cor e largura da borda
+      cv2.rectangle(cv2_frame, (x,y), (x + largura, y + largura), (0, 0 , 255), 2)
     # convert img to ros and pub image
     ros_frame = self.bridge.cv2_to_imgmsg(cv2_frame, "bgr8")
     self.pub.publish(ros_frame)
 
   def listener(self):
     # subscribe to a topic
-    rospy.Subscriber('depth_camera/rgb/image_raw', Image, self.callback)  
+    rospy.Subscriber('depth_camera/rgb/image_raw', Image, self.callback)
     # simply keeps python from exiting until this node is stopped
     rospy.spin()
 
